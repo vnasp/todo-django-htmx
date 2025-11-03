a5407ae08f624f7578e13decaee9fa585cf17c27
Valentina Muñoz <valentinamr@gmail.com>
2025-11-03 17:43:44 -0300

    fix edit task in render deploy

--- tasks/api_views.py
+++ tasks/api_views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, status, mixins
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from django.template.loader import render_to_string
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -23,6 +24,41 @@ class TaskViewSet(mixins.ListModelMixin,
     queryset = Task.objects.all().order_by('created_at')
     serializer_class = TaskSerializer
     
+    def get_response_for_htmx(self):
+        """Retorna HTML renderizado para peticiones HTMX"""
+        pending_tasks = Task.objects.filter(completed=False, deleted=False).order_by("-created_at")
+        completed_tasks = Task.objects.filter(completed=True, deleted=False).order_by("-created_at")
+        deleted_tasks = Task.objects.filter(deleted=True).order_by("-created_at")
+        
+        context = {
+            'pending_tasks': pending_tasks,
+            'completed_tasks': completed_tasks,
+            'deleted_tasks': deleted_tasks,
+        }
+        html = render_to_string('partials/all_columns.html', context)
+        return Response(html, content_type='text/html')
+    
+    def create(self, request, *args, **kwargs):
+        """POST - Crear tarea"""
+        response = super().create(request, *args, **kwargs)
+        if request.headers.get('HX-Request'):
+            return self.get_response_for_htmx()
+        return response
+    
+    def update(self, request, *args, **kwargs):
+        """PUT - Actualizar tarea"""
+        response = super().update(request, *args, **kwargs)
+        if request.headers.get('HX-Request'):
+            return self.get_response_for_htmx()
+        return response
+    
+    def destroy(self, request, *args, **kwargs):
+        """DELETE - Eliminar tarea"""
+        response = super().destroy(request, *args, **kwargs)
+        if request.headers.get('HX-Request'):
+            return self.get_response_for_htmx()
+        return response
+    
     @action(detail=True, methods=['post'])
     def toggle(self, request, pk=None):
         """
@@ -32,5 +68,7 @@ class TaskViewSet(mixins.ListModelMixin,
         task = self.get_object()
         task.completed = not task.completed
         task.save()
+        if request.headers.get('HX-Request'):
+            return self.get_response_for_htmx()
         serializer = self.get_serializer(task)
         return Response(serializer.data)
